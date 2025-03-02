f689400021a1030f0419706d3f018b7d0d8d27f1
gordoly <gordonly@Gordons-Laptop.local>
2025-03-02 22:53:11 +1100

    updated application.properties and ChessApplication.java to enable database connection

--- server/src/main/java/com/example/Chess/ChessApplication.java
+++ server/src/main/java/com/example/Chess/ChessApplication.java
@@ -2,8 +2,12 @@ package com.example.Chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.Chess.Repository")
+@EntityScan(basePackages = "com.example.Chess.Model")
 public class ChessApplication {
 
 	public static void main(String[] args) {
